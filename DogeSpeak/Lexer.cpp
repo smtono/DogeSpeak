b3691b7cf7af187974eb341173d3d5d3ee6c4454
smtono <64560893+smtono@users.noreply.github.com>
2021-04-06 02:27:37 -0500

    added declaration and implementation files for lexer

---
+++ DogeSpeak/Lexer.cpp
@@ -0,0 +1,16 @@
+/**
+	Contains all the lexical analysis for DogeSpeak
+	when using it in conjuction with the Token class.
+
+	Based on code found at https://gist.github.com/arrieta/1a309138689e09375b90b3b1aa768e20
+
+	@file Token.cpp
+	@author Shannon Thornton
+	@version 1.0
+*/
+
+#include "Lexer.h"
+
+Lexer::Lexer() {
+
+}
