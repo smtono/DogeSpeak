b045bc036bb2136ae307bb4a3c4cece06c164705
smtono <64560893+smtono@users.noreply.github.com>
2021-04-16 23:22:22 -0500

    update token

---
+++ src/Lexer/Token/Token.java
@@ -0,0 +1,28 @@
+package Lexer.Token;
+
+public class Token {
+    // TOKEN ATTRIBUTES
+    private final TokenType type;
+    private final Object value;
+
+    // CONSTRUCTORS
+    public Token(TokenType type, Object value) {
+        this.type = type;
+        this.value = value;
+    }
+    public Token(TokenType type) {
+        this.type = type;
+        this.value = null;
+    }
+
+    // ACCESSORS
+    public TokenType getType() { return type; }
+    public Object getValue() { return value; }
+
+    // HELPER METHODS\
+
+    @Override
+    public String toString() {
+        return type + " " + value;
+    }
+}
