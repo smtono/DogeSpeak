3b8672457bbc69d6a46448129ee76d2a07d763a1
smtono <64560893+smtono@users.noreply.github.com>
2021-04-19 22:30:40 -0500

    added new error

---
+++ src/Error/CapitalCheckError.java
@@ -0,0 +1,10 @@
+package Error;
+
+public class CapitalCheckError extends Error {
+
+    public CapitalCheckError(String errorName, String details) {
+        super(errorName, details);
+    }
+    
+    
+}
