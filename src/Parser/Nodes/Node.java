99ceda82f3ae6b78040d906a5028c0641956af39
smtono <64560893+smtono@users.noreply.github.com>
2021-04-23 03:10:44 -0500

    added node type support to node

--- src/Parser/Nodes/Node.java
+++ src/Parser/Nodes/Node.java
@@ -10,12 +10,34 @@ import Lexer.Token.TokenType;
 public class Node {
     // ATTRIBUTES
     private final Token token;
+    private final NodeType nodeType;
+
 
     // CONSTRUCTOR
-    public Node() {this.token = new Token(TokenType.NONE);} // default (for variable instantiation)
+    public Node() { // default (for variable instantiation)
+        this.token = new Token(TokenType.NONE);
+        this.nodeType = NodeType.NONE;
+    }
+
     public Node(Token token) {
         this.token = token;
+
+        switch (token.getType()) {
+            case INTEGER:
+            case FLOAT:
+                this.nodeType = NodeType.NUMBER;
+                break;
+            case OPERATOR:
+                this.nodeType = NodeType.ARITHMETIC_OPERATION;
+                break;
+            default:
+                this.nodeType = NodeType.NONE;
+        }
     }
+
+    // ACCESSORS
+    public Token getToken() { return token; }
+    public NodeType getNodeType() { return nodeType; }
 
     @Override
     public String toString() {
