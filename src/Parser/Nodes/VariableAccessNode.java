53f371b86322a14b9d484f444f451ea48ec69ed7
smtono <64560893+smtono@users.noreply.github.com>
2021-04-25 04:12:20 -0500

    created variable nodes

--- src/Parser/Nodes/VariableAccessNode.java
+++ src/Parser/Nodes/VariableAccessNode.java
@@ -1,5 +1,22 @@
 package Parser.Nodes;
 
+import Lexer.Token.Token;
+
 public class VariableAccessNode extends Node {
-    
+    // ATTRIBUTES
+    private final Token token;
+
+    // CONSTRUCTOR
+    public VariableAccessNode(Token token) {
+        this.token = token;
+    }
+
+    // ACCESSORS
+    @Override
+    public Token getToken() { return token; }
+
+    @Override
+    public String toString() {
+        return token.toString();
+    }
 }
--- src/Parser/Nodes/VariableAssignmentNode.java
+++ src/Parser/Nodes/VariableAssignmentNode.java
@@ -1,11 +1,29 @@
 package Parser.Nodes;
 
+import Interpreter.GenericNumber;
+import Lexer.Token.Token;
+
 public class VariableAssignmentNode extends Node {
     // ATTRIBUTES
-    
+    private final Token name;
+    private final Node value;
 
     // CONSTRUCTOR
-    public VariableAssignmentNode() {
-
+    public VariableAssignmentNode(Token name, Node value) {
+        super(name);
+        this.name = name;
+        this.value = value;
     }
+
+    // ACCESSORS
+    public Token getName() { return name; }
+    public Node getValue() { return value; }
+
+    @Override
+    public GenericNumber evaluateExpression() {
+       return getValue().evaluateExpression();
+    }
+
+    @Override
+    public String toString() { return value.toString(); }
 }
